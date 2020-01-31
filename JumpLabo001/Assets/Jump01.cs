3c8e092673c38f62e92f3c7c50f0f10fbc63325d
mswar777 <mswar777@gmail.com>
2020-01-31 14:54:44 +0900

    test comit.

---
+++ JumpLabo001/Assets/Jump01.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //force = 4.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Transform.Position.y += force;
+        //force -= 0.1f;
+    }
+
+    //float force;
+}
