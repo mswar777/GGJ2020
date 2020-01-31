2a45e2b4a5740cf28056fe9dbda80b726662f112
mswar777 <mswar777@gmail.com>
2020-02-01 02:15:04 +0900

    Add 2D Project.

---
+++ JumpLabo002/Assets/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.localPosition;
+        pos.y += velocity;
+        transform.localPosition = pos;
+        if (transform.localPosition.y > 0)
+        {
+            velocity -= 0.2f;
+        }
+        else
+        {
+            pos.y = 0;
+            transform.localPosition = pos;
+            velocity = 10;
+        }
+    }
+}
